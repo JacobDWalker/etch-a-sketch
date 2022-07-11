35e82526d911dfa680bff222dc14b28923076db1
JacobDWalker <jacobdanwalker@gmail.com>
2022-07-10 22:34:30 -0400

    Add UI and sketch functionality
    Complete UI creation and sketch functionality. User is now able to
    choose black, rainbow, or erase for grid coloring options. Add button
    to clear grid and replace it with a new one.

--- logic.js
+++ logic.js
@@ -1,24 +1,60 @@
+function createGrid(userGridSize) {
+    gridContainerDiv.textContent = "";
+    let gridSide = gridContainerDiv.offsetHeight / userGridSize;
+    let gridArea = userGridSize * userGridSize;
+    for (let i = 0; i < gridArea; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.style.width = gridSide.toString();
+        newDiv.style.height = gridSide.toString();
+        newDiv.ondragstart = () => false;
+        newDiv.addEventListener('mousedown', () => changeColor(newDiv));
+        newDiv.addEventListener('mouseover', () => {
+            if (mouseDown) {
+                changeColor(newDiv)
+            }
+        })
+        gridContainerDiv.appendChild(newDiv);
+    }
+}
 
+function getRandomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${blue}, ${green})`;
+}
+
+
+function changeColor(div) {
+    if (selectedColor === 'black') {
+        div.style.backgroundColor = 'black';
+    } else if ( selectedColor === 'rainbow') {
+        div.style.backgroundColor = getRandomColor();
+    } else {
+        div.style.backgroundColor = 'white';
+    }
+}
+let selectedColor = 'black';
+const colorSelector = document.getElementById('color-select');
+colorSelector.addEventListener('change', () => selectedColor = colorSelector.value);
+
+// Clear grid button
+document.getElementById('clear-grid').addEventListener('click', () => createGrid(slider.value));
+
+let mouseDown = false;
+const slider = document.getElementById('slider');
+const sliderDisplay = document.getElementById('slider-value-display');
+sliderDisplay.textContent = `${slider.value} X ${slider.value}`
 const gridContainerDiv = document.getElementById('grid-container');
 
-function createGrid() {
-    let gridSize = Number(prompt("How big should each side of the grid be?"));
+createGrid(slider.value);
 
-    if (gridSize > 100 || gridSize < 1) {
-        alert("Grid size too large");
-    } else {
-        gridContainerDiv.textContent = "";
-        let gridSide = gridContainerDiv.offsetHeight / gridSize;
-        for (let i = 0; i < gridSize * gridSize; i++) {
-            let newDiv = document.createElement('div');
-            // newDiv.style.outline = 'auto';
-            newDiv.style.width = gridSide.toString();
-            newDiv.style.height = gridSide.toString();
-            gridContainerDiv.appendChild(newDiv);
-        }
-        console.log("done");
-    }
+// Event listeners to allow dragging stencil
+gridContainerDiv.onmousedown = () => mouseDown = true;
+gridContainerDiv.onmouseup = () => mouseDown = false;
 
-
-
-}
+// Allow user to set grid via slider
+slider.addEventListener('click', () => {
+    sliderDisplay.textContent = `${slider.value} X ${slider.value}`;
+    createGrid(slider.value);
+})
